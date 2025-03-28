d86d8696e6e386c6fe929ebb9a6dc8dc94211efb
Alex Corcho <alexander.corcho@gmail.com>
2025-03-28 16:43:04 -0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API funcionando perfectamente!',
+        'status' => 'success'
+    ]);
+});
+
+Route::get('/productos', function () {
+    return response()->json([
+        'message' => 'todos los productos',
+        'status' => 'success'
+    ]);
+
+});
